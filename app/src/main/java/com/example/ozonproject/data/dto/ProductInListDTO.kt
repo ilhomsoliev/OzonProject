613a1ec3c457e675b6f54fd06dfc593e48df0598
Ilhom <solievoliver@gmail.com>
2022-06-08 18:57:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/ozonproject/data/dto/ProductInListDTO.kt
@@ -0,0 +1,11 @@
+package com.example.ozonproject.data.dto
+
+data class ProductInListDTO(
+    val guid: String,
+    val image: String,
+    val name: String,
+    val price: String,
+    val rating: Double,
+    val isFavorite: Boolean,
+    val isInCart: Boolean
+)
---
+++ app/src/main/java/com/example/ozonproject/domain/interactors/ProductsInteractor.kt
@@ -0,0 +1,10 @@
+
+package ru.ozon.route256.workshop1.domain.interactors
+
+import ru.ozon.route256.workshop1.presentation.viewObject.ProductInListVO
+
+interface ProductsInteractor {
+    fun getProducts(): List<ProductInListVO>
+
+    fun getProductById(guid: String): ProductInListVO
+}
